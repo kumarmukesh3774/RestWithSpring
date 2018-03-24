1ca17f53b4d4c227535536975e9c1088b773341e
kumarmukesh3774 <kumarmukesh3774@gmail.com>
2018-03-24 20:09:41 +0530

    23

--- src/main/java/com/example/rest/user/User.java
+++ src/main/java/com/example/rest/user/User.java
@@ -5,12 +5,19 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+
+@ApiModel(description="All Details about the User")
 public class User {
 
 	Integer id;
 	@Size(min=2,message="Name Should have at least 2 characters")
+	@ApiModelProperty(notes="Name Should have at least 2 characters")
 	String name;
 	@Past
+	@ApiModelProperty(notes="DOB Should be Past date")
 	Date dob;
 	
 	public User() {
